37e19e7cedeae95c34d53f93765e834758135554
hija <hilko.j@wwu.de>
2022-09-11 00:00:23 +0200

    added cleanup

--- main.py
+++ main.py
@@ -52,6 +52,7 @@ def _print_table_with_status(fp_stati: list[FotoparadiesStatus]):
 @app.command()
 def status():
     current_list = get_orders_list()
+    ### TODO: SLIDER
     for fp_status in current_list:
         fp_status.refresh()
 
@@ -66,10 +67,14 @@ def add(shop: int, order: int, name: Optional[str] = typer.Argument(None)):
 
     for elem in current_list:
         if elem._order == order and elem._shop == shop:
-            console.log(
+            console.print(
                 f"Der Auftrag befindet sich bereits unter dem Name {elem.ordername} in der Liste!"
             )
             return
+        if elem.ordername == name:
+            console.print(
+                "Ein Auftrag mit identischem Namen befindet sich bereits in der Liste. Ein weiterer Auftrag muss einen anderen Namen haben."
+            )
 
     current_list.append(fp_status)
     save_orders_list(current_list)
@@ -88,6 +93,24 @@ def remove(name: str):
     console.print("Der Auftrag wurde nicht gefunden.")
 
 
+@app.command()
+def cleanup():
+    removed_entries = 0
+
+    current_list = get_orders_list()
+    for elem in current_list:
+        if elem.currentstatus and elem.currentstatus == "DELIVERED":
+            current_list.remove(elem)
+            removed_entries += 1
+    save_orders_list(current_list)
+
+    if removed_entries > 0:
+        console.print(
+            f"Es wurde {removed_entries}, die bereits geliefert wurden, gelöscht."
+        )
+    else:
+        console.print("Es gab keinen Auftrag, der gelöscht werden konnte.")
+
+
 if __name__ == "__main__":
-    print(str(_get_orders_filepath()))
     app()
